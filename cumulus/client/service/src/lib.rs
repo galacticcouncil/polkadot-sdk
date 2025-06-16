72756f845dccd02de8ec43adf6cc068c6a34c75e
Roznovjak <richardroznovjak@gmail.com>
2025-06-16 12:50:08 +0200

    Support for tx priority
    initial impl
    
    add tx parameter
    
    refactoring
    
    change string type
    
    new type for tx details
    
    change string type in TransactionDetail struct
    
    public fields in TransactionDetail
    
    transaction detail changes
    
    remove struct for substrate tx details
    
    docs and cleanup
    
    rename struct field
    
    also use signer and call_address when comparing with tx details
    
    use json data from a string instead of Path to the file with tx priorities
    
    add comment and change logs
    
    remove test
    
    patch fork-aware tx pool for tx priority
    
    Update substrate/client/service/src/client/client.rs
    
    Co-authored-by: Martin Hloska <martin.hloska@gmail.com>
    
    resolve comments from the review
    
    rename TransactionPriorityModule to TransactionPriorityModifier

--- cumulus/client/service/src/lib.rs
+++ cumulus/client/service/src/lib.rs
@@ -445,6 +445,7 @@ pub struct BuildNetworkParams<
 		+ HeaderMetadata<Block, Error = sp_blockchain::Error>
 		+ HeaderBackend<Block>
 		+ BlockIdTo<Block>
+		+ sp_blockchain::TransactionPriorityModifierT<Block = Block>
 		+ 'static,
 	Network: NetworkBackend<Block, <Block as BlockT>::Hash>,
 	RCInterface,
@@ -498,6 +499,7 @@ where
 		+ HeaderMetadata<Block, Error = sp_blockchain::Error>
 		+ BlockIdTo<Block, Error = sp_blockchain::Error>
 		+ ProofProvider<Block>
+		+ sp_blockchain::TransactionPriorityModifierT<Block = Block>
 		+ 'static,
 	Client::Api: CollectCollationInfo<Block>
 		+ sp_transaction_pool::runtime_api::TaggedTransactionQueue<Block>,
